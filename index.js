a01d7ac17bb5af43a2a695847bc7dc4476552a04
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2025-02-05 17:58:34 +1100

    feat: Add preacher capability

--- index.js
+++ index.js
@@ -6,25 +6,33 @@ import TTLCache from '@isaacs/ttlcache';
 
 dotenv.config()
 
-const getLatestOffering = async () => {
-  var client = new google.auth.JWT(
-    process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    null,
-    Buffer.from(process.env.GOOGLE_PRIVATE_KEY, 'base64').toString('utf-8'),
-    ['https://www.googleapis.com/auth/spreadsheets'],
-    null,
-    process.env.GOOGLE_PRIVATE_KEY_ID
-  )
-  await client.authorize()
-  var service = google.sheets({
-    version: 'v4',
-    auth: client,
-  })
+var client = new google.auth.JWT(
+  process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  null,
+  Buffer.from(process.env.GOOGLE_PRIVATE_KEY, 'base64').toString('utf-8'),
+  ['https://www.googleapis.com/auth/spreadsheets'],
+  null,
+  process.env.GOOGLE_PRIVATE_KEY_ID
+)
+await client.authorize()
+var service = google.sheets({
+  version: 'v4',
+  auth: client,
+})
 
+const getValue = async (cell) => {
   return (await service.spreadsheets.values.get({
     spreadsheetId: "1jzCZQFueFo6hHRZ0apPrc8InIHZhSnJaFTxuVqVIvn0",
-    range: `C18`,
+    range: cell,
   })).data.values[0][0]
+}
+
+const getLatestOffering = async () => {
+  return await getValue('Reminders!B8')
+}
+
+const getPreacher = async () => {
+  return await getValue('Reminders!B3')
 }
 
 const time = 30 * 60 * 1000
@@ -34,8 +42,7 @@ const app = new Koa()
 app.use(koaCash({
   maxAge: time,
   get: (key) => cache.get(key),
-  set: (key, value) => cache.set(key, value),
-  hash: () => 1,
+  set: (key, value) => cache.set(key, value)
 }))
 
 app.use(async (ctx) => {
@@ -45,6 +52,11 @@ app.use(async (ctx) => {
   }
   if (ctx.request.query.type !== 'refresh' && await ctx.cashed()) return;
 
+  if (ctx.path == '/api/preacher') {
+    ctx.body = await getPreacher() || "Unknown"
+    return
+  }
+  
   ctx.type = 'application/rss+xml'
   ctx.body = `<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0"><channel><title>Wollongong Seventh-day Adventist Church</title><link>https://www.illawarraadventist.org/</link><description>Wollongong Seventh-day Adventist Church</description><item><title>${await getLatestOffering() || "Unknown"}</title><link>https://www.illawarraadventist.org/</link><description></description></item></channel></rss>`
 })
